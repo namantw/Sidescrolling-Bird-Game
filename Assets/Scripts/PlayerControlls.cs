72e9ecfe5472386aed77a369eb63282a06377af8
Naman Tiwari <31323328+namantw@users.noreply.github.com>
2017-12-15 03:26:53 +0530

    Add files via upload

---
+++ Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlls : MonoBehaviour {
+    public Rigidbody2D rb;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.velocity = new Vector2(5, rb.velocity.y);
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if(Input.GetMouseButtonDown(0) && onGround)
+        {
+            rb.AddForce(new Vector2(0, 500f));
+        }
+
+        
+
+    }
+}
